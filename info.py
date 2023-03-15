f1fde6a2256e1813219f92408fb6faabc02d7f2f
Silent0070 <92304473+Silent0070@users.noreply.github.com>
2023-03-15 15:21:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,7 +80,7 @@ AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
 MAUTO_DELETE = is_enabled((environ.get('MAUTO_DELETE', "True")), True)
 
 # Delete Time
-DELETE_TIME = int(environ.get('DELETE_TIME', 100))
+DELETE_TIME = int(environ.get('DELETE_TIME', 120))
 SPL_DELETE_TIME = int(environ.get('SPL_DELETE_TIME', 15))
 
 # URL SHORTNER
